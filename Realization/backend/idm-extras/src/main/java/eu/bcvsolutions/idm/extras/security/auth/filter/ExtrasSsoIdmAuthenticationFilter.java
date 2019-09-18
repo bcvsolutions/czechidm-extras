39566097dee02278108751466a9a97b534c81412
Kolychev Artem <perec.pochta@gmail.com>
2019-09-18 09:50:30 +0200

    #18564 added extend for sso auth filter.

---
+++ Realization/backend/idm-extras/src/main/java/eu/bcvsolutions/idm/extras/security/auth/filter/ExtrasSsoIdmAuthenticationFilter.java
@@ -0,0 +1,95 @@
+package eu.bcvsolutions.idm.extras.security.auth.filter;
+
+import com.google.common.base.Strings;
+import eu.bcvsolutions.idm.core.api.dto.AbstractDto;
+import eu.bcvsolutions.idm.core.api.dto.IdmIdentityContractDto;
+import eu.bcvsolutions.idm.core.api.dto.IdmIdentityDto;
+import eu.bcvsolutions.idm.core.api.service.LookupService;
+import eu.bcvsolutions.idm.core.eav.service.impl.DefaultFormService;
+import eu.bcvsolutions.idm.core.model.entity.IdmIdentity;
+import eu.bcvsolutions.idm.core.model.entity.IdmIdentityContract;
+import eu.bcvsolutions.idm.core.security.auth.filter.SsoIdmAuthenticationFilter;
+import eu.bcvsolutions.idm.core.security.exception.IdmAuthenticationException;
+import java.text.MessageFormat;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
+
+public abstract class ExtrasSsoIdmAuthenticationFilter extends SsoIdmAuthenticationFilter {
+	private static final Logger LOG = LoggerFactory.getLogger(SsoIdmAuthenticationFilter.class);
+	static final String PARAMETER_FIELDS = "fields";
+
+	@Autowired
+	private LookupService lookupService;
+	@Autowired
+	private DefaultFormService formService;
+
+	@Override
+	public boolean authorize(String token, HttpServletRequest request, HttpServletResponse response) {
+
+		if (super.authorize(token, request, response)) {
+			return true;
+		}
+
+		LOG.debug("Starting SSO filter authorization, value of the SSO header is: [{}]", token);
+		if (Strings.isNullOrEmpty(token)) {
+			return true;
+		}
+		// Remove suffix from the token - typically the domain
+		String userName = removeUidSuffix(token);
+		Set<UUID> uuids = new LinkedHashSet<>();
+
+		List<String> fieldNames = getConfigurationService()
+				.getValues(getConfigurationPropertyName(PARAMETER_FIELDS));
+
+		for (String fieldName : fieldNames) {
+			if (StringUtils.isEmpty(fieldName)) {
+				return false;
+			}
+			Page<AbstractDto> owners;
+
+			try {
+				owners = formService.findOwners(IdmIdentityContract.class, fieldName, userName, null);
+				for (AbstractDto owner : owners) {
+					uuids.add(((IdmIdentityContractDto) owner).getIdentity());
+				}
+			} catch (IllegalArgumentException ignored) {
+			}
+
+			try {
+				owners = formService.findOwners(IdmIdentity.class, fieldName, userName, null);
+				for (AbstractDto owner : owners) {
+					uuids.add(owner.getId());
+				}
+			} catch (IllegalArgumentException ignored) {
+			}
+		}
+
+		if (!uuids.isEmpty()) {
+			if (uuids.size() > 1) {
+				throw new IdmAuthenticationException(MessageFormat.format(
+						"The login [{0}] is not unique. SSO authentication is not passable.",
+						userName));
+			}
+			IdmIdentityDto identity = (IdmIdentityDto) lookupService.lookupDto(IdmIdentityDto.class, uuids.iterator().next());
+			if (null == identity) {
+				return false;
+			}
+			return super.authorize(identity.getUsername(), request, response);
+		}
+
+		return false;
+	}
+
+
+
+}
---
+++ Realization/backend/idm-extras/src/main/java/eu/bcvsolutions/idm/extras/security/auth/filter/ExtrasSsoIdmAuthenticationFilterTest.java
@@ -0,0 +1,179 @@
+package eu.bcvsolutions.idm.extras.security.auth.filter;
+
+import com.google.common.collect.Lists;
+import eu.bcvsolutions.idm.core.api.dto.IdmIdentityContractDto;
+import eu.bcvsolutions.idm.core.api.dto.IdmIdentityDto;
+import eu.bcvsolutions.idm.core.api.service.IdmConfigurationService;
+import eu.bcvsolutions.idm.core.api.utils.AutowireHelper;
+import eu.bcvsolutions.idm.core.eav.api.domain.PersistentType;
+import eu.bcvsolutions.idm.core.eav.api.dto.IdmFormAttributeDto;
+import eu.bcvsolutions.idm.core.eav.api.service.FormService;
+import eu.bcvsolutions.idm.core.model.entity.IdmIdentity;
+import eu.bcvsolutions.idm.core.model.entity.IdmIdentityContract;
+import eu.bcvsolutions.idm.core.security.auth.filter.SsoIdmAuthenticationFilter;
+import eu.bcvsolutions.idm.core.security.exception.IdmAuthenticationException;
+import eu.bcvsolutions.idm.test.api.AbstractIntegrationTest;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+
+/**
+ * Test authentication using SSO.
+ *
+ * @author Artem Kolychev
+ */
+@Transactional
+public class ExtrasSsoIdmAuthenticationFilterTest extends AbstractIntegrationTest {
+
+	private static final String TEST_SAME_SERVICE_LOGIN_FIELD = "sameServiceLoginField";
+	private static final String TEST_SAME_SERVICE_LOGIN = "sameServiceLogin";
+
+	private static final String PARAMETER_UID_SUFFIXES = "@domain.tld";
+
+	@Autowired
+	private IdmConfigurationService configurationService;
+	@Autowired
+	private FormService formService;
+	private ExtrasSsoIdmAuthenticationFilter filter;
+
+	@Before
+	public void init() {
+		filter = AutowireHelper.createBean(ExtrasSsoIdmAuthenticationFilter.class);
+		configurationService.setValue(
+				filter.getConfigurationPropertyName(
+						ExtrasSsoIdmAuthenticationFilter.PARAMETER_FIELDS),
+				TEST_SAME_SERVICE_LOGIN_FIELD
+		);
+		configurationService.setValue(
+				filter.getConfigurationPropertyName(
+						SsoIdmAuthenticationFilter.PARAMETER_UID_SUFFIXES),
+				PARAMETER_UID_SUFFIXES
+		);
+	}
+
+	@After
+	public void after() {
+		// reset to default
+		this.logout();
+	}
+
+	@Test
+	public void testSsoAuthUserWithFormField() {
+		IdmIdentityDto identity = getHelper().createIdentity();
+		IdmIdentityContractDto primeContract = getHelper().getPrimeContract(identity.getId());
+		//
+		IdmFormAttributeDto eavAttributeContract = getHelper().createEavAttribute(
+				TEST_SAME_SERVICE_LOGIN_FIELD,
+				IdmIdentityContract.class,
+				PersistentType.SHORTTEXT
+		);
+		getHelper().setEavValue(
+				primeContract,
+				eavAttributeContract,
+				IdmIdentityContract.class,
+				TEST_SAME_SERVICE_LOGIN,
+				PersistentType.SHORTTEXT
+		);
+		//
+		Assert.assertTrue(filter.authorize(TEST_SAME_SERVICE_LOGIN + PARAMETER_UID_SUFFIXES, null, null));
+	}
+
+
+	@Test
+	public void testSsoAuthUserWithFormContractMultiValueField() {
+		IdmFormAttributeDto eavAttributeContract = getHelper().createEavAttribute(
+				TEST_SAME_SERVICE_LOGIN_FIELD,
+				IdmIdentityContract.class,
+				PersistentType.SHORTTEXT);
+		eavAttributeContract.setMultiple(true);
+
+		IdmIdentityDto identity = getHelper().createIdentity();
+		IdmIdentityContractDto primeContract1 = getHelper().getPrimeContract(identity.getId());
+		formService.saveValues(
+				primeContract1.getId(), primeContract1.getClass(), eavAttributeContract, Lists.newArrayList(TEST_SAME_SERVICE_LOGIN, PARAMETER_UID_SUFFIXES));
+
+		Assert.assertTrue(filter.authorize(TEST_SAME_SERVICE_LOGIN + PARAMETER_UID_SUFFIXES, null, null));
+	}
+	@Test
+	public void testSsoAuthUserWithFormIdentityMultiValueField() {
+		IdmFormAttributeDto eavAttributeIdentity = getHelper().createEavAttribute(
+				TEST_SAME_SERVICE_LOGIN_FIELD,
+				IdmIdentity.class,
+				PersistentType.SHORTTEXT);
+		eavAttributeIdentity.setMultiple(true);
+
+		IdmIdentityDto identity = getHelper().createIdentity();
+		formService.saveValues(
+				identity.getId(),
+				identity.getClass(),
+				eavAttributeIdentity,
+				Lists.newArrayList(TEST_SAME_SERVICE_LOGIN, PARAMETER_UID_SUFFIXES));
+
+		Assert.assertTrue(filter.authorize(TEST_SAME_SERVICE_LOGIN + PARAMETER_UID_SUFFIXES, null, null));
+	}
+
+	@Test(expected = IdmAuthenticationException.class)
+	public void testSsoAuthUserWithDuplicatedFormFieldDifferentPeople() {
+
+		IdmFormAttributeDto eavAttributeContract = getHelper().createEavAttribute(
+				TEST_SAME_SERVICE_LOGIN_FIELD,
+				IdmIdentityContract.class,
+				PersistentType.SHORTTEXT);
+
+		IdmIdentityDto identity = getHelper().createIdentity();
+		IdmIdentityContractDto primeContract1 = getHelper().getPrimeContract(identity.getId());
+		getHelper().setEavValue(
+				primeContract1,
+				eavAttributeContract,
+				IdmIdentityContract.class,
+				TEST_SAME_SERVICE_LOGIN,
+				PersistentType.SHORTTEXT
+		);
+
+		IdmIdentityDto identity2 = getHelper().createIdentity();
+		IdmIdentityContractDto primeContract2 = getHelper().getPrimeContract(identity2.getId());
+		getHelper().setEavValue(
+				primeContract2,
+				eavAttributeContract,
+				IdmIdentityContract.class,
+				TEST_SAME_SERVICE_LOGIN,
+				PersistentType.SHORTTEXT
+		);
+
+		//
+		filter.authorize(TEST_SAME_SERVICE_LOGIN + PARAMETER_UID_SUFFIXES, null, null);// Wait exception.
+	}
+
+	@Test
+	public void testSsoAuthUserWithDuplicatedFormFieldSamePeople() {
+		IdmIdentityDto identity = getHelper().createIdentity();
+		IdmIdentityContractDto primeContract = getHelper().getPrimeContract(identity.getId());
+		IdmIdentityContractDto primeContract2 = getHelper().getPrimeContract(identity.getId());
+		//
+		IdmFormAttributeDto eavAttributeContract;
+		eavAttributeContract = getHelper()
+				.createEavAttribute(TEST_SAME_SERVICE_LOGIN_FIELD, IdmIdentityContract.class, PersistentType.SHORTTEXT);
+
+		getHelper().setEavValue(primeContract,
+				eavAttributeContract,
+				IdmIdentityContract.class,
+				TEST_SAME_SERVICE_LOGIN,
+				PersistentType.SHORTTEXT
+		);
+
+		getHelper().setEavValue(primeContract2,
+				eavAttributeContract,
+				IdmIdentityContract.class,
+				TEST_SAME_SERVICE_LOGIN,
+				PersistentType.SHORTTEXT
+		);
+
+		//
+		Assert.assertTrue(filter.authorize(TEST_SAME_SERVICE_LOGIN + PARAMETER_UID_SUFFIXES, null, null));
+	}
+
+}
