cd616d17d0cd529f11de1d54a0fca18b39056331
klementm <marek.klement@bcvsolutions.eu>
2019-09-18 12:06:31 +0200

    [#1842] created test - tests if task was succesful

--- Realization/backend/idm-extras/src/test/java/eu/bcvsolutions/idm/extras/scheduler/task/impl/CzechIdMStatusNotificationTaskTest.java
+++ Realization/backend/idm-extras/src/test/java/eu/bcvsolutions/idm/extras/scheduler/task/impl/CzechIdMStatusNotificationTaskTest.java
@@ -2,34 +2,48 @@ package eu.bcvsolutions.idm.extras.scheduler.task.impl;
 
 import static org.junit.Assert.*;
 
-import java.time.chrono.ChronoZonedDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.joda.time.DateTime;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import eu.bcvsolutions.idm.acc.service.api.SysSyncConfigService;
+import eu.bcvsolutions.idm.acc.service.api.SysSystemMappingService;
+import eu.bcvsolutions.idm.core.api.domain.OperationState;
 import eu.bcvsolutions.idm.core.api.dto.IdmIdentityDto;
-import eu.bcvsolutions.idm.core.api.service.ConfigurationService;
+import eu.bcvsolutions.idm.core.scheduler.api.dto.IdmLongRunningTaskDto;
 import eu.bcvsolutions.idm.core.scheduler.api.service.LongRunningTaskManager;
-import eu.bcvsolutions.idm.test.api.AbstractIntegrationTest;
-import eu.bcvsolutions.idm.test.api.TestHelper;
+import eu.bcvsolutions.idm.extras.TestHelper;
+import eu.bcvsolutions.idm.test.api.AbstractNotificationTest;
 
-public class CzechIdMStatusNotificationTaskTest extends AbstractIntegrationTest {
+public class CzechIdMStatusNotificationTaskTest extends AbstractNotificationTest {
 
 	@Autowired
 	private TestHelper testHelper;
 	@Autowired
 	private LongRunningTaskManager longRunningTaskManager;
 	@Autowired
-	private ConfigurationService configurationService;
+	private CzechIdMStatusNotificationTask lrt;
+	@Autowired
+	private SysSyncConfigService configService;
+	@Autowired
+	private SysSystemMappingService mappingService;
+
+	@Before
+	public void init(){
+		testHelper.loginAdmin();
+	}
+
+	@After
+	public void logout(){
+		testHelper.logout();
+	}
 
 	@Test
 	public void testSendNotification(){
-		configurationService.setValue(CzechIdMStatusNotificationTask.LAST_RUN_DATE_TIME, DateTime.now().minusDays(10).toString());
 		IdmIdentityDto identity = testHelper.createIdentity();
-		CzechIdMStatusNotificationTask lrt = new CzechIdMStatusNotificationTask();
 		// create setting of lrt
 		Map<String, Object> configOfLRT = new HashMap<>();
 		configOfLRT.put(CzechIdMStatusNotificationTask.SEND_CONTRACTS_STATUS_PARAM, "true");
@@ -40,6 +54,9 @@ public class CzechIdMStatusNotificationTaskTest extends AbstractIntegrationTest 
 		configOfLRT.put(CzechIdMStatusNotificationTask.RECIPIENTS_PARAM, identity.getId().toString());
 		lrt.init(configOfLRT);
 		Boolean obj = longRunningTaskManager.executeSync(lrt);
+		assertTrue(obj);
+		IdmLongRunningTaskDto task = longRunningTaskManager.getLongRunningTask(lrt);
+		assertEquals(OperationState.EXECUTED, task.getResult().getState());
 	}
 
 }
