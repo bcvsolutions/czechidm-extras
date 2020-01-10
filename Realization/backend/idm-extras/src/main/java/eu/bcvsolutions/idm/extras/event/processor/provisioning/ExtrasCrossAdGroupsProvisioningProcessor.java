08a2e6e8dbc94870b432accd103cb99667bb77b5
Roman Kučera <roman.kuca@gmail.com>
2020-01-10 09:57:15 +0100

    #19353 code cleanup and refactor. Fixed logic for getting all groups and sending them to connector. Sending groups to add and to remove as two separate lists for better handling in connector and to reduce calling end system in connector again

--- Realization/backend/idm-extras/src/main/java/eu/bcvsolutions/idm/extras/event/processor/provisioning/ExtrasCrossAdGroupsProvisioningProcessor.java
+++ Realization/backend/idm-extras/src/main/java/eu/bcvsolutions/idm/extras/event/processor/provisioning/ExtrasCrossAdGroupsProvisioningProcessor.java
@@ -1,6 +1,7 @@
 package eu.bcvsolutions.idm.extras.event.processor.provisioning;
 
 import java.io.Serializable;
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
@@ -21,33 +22,33 @@ import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
 
 import eu.bcvsolutions.idm.acc.AccModuleDescriptor;
-import eu.bcvsolutions.idm.acc.config.domain.ProvisioningConfiguration;
 import eu.bcvsolutions.idm.acc.domain.AccResultCode;
 import eu.bcvsolutions.idm.acc.domain.ProvisioningEventType;
+import eu.bcvsolutions.idm.acc.domain.SystemEntityType;
+import eu.bcvsolutions.idm.acc.domain.SystemOperationType;
 import eu.bcvsolutions.idm.acc.dto.ProvisioningAttributeDto;
 import eu.bcvsolutions.idm.acc.dto.SysProvisioningOperationDto;
+import eu.bcvsolutions.idm.acc.dto.SysSchemaAttributeDto;
+import eu.bcvsolutions.idm.acc.dto.SysSchemaObjectClassDto;
 import eu.bcvsolutions.idm.acc.dto.SysSystemDto;
 import eu.bcvsolutions.idm.acc.dto.SysSystemEntityDto;
+import eu.bcvsolutions.idm.acc.dto.SysSystemMappingDto;
+import eu.bcvsolutions.idm.acc.dto.filter.SysSchemaAttributeFilter;
+import eu.bcvsolutions.idm.acc.entity.SysSchemaAttribute;
 import eu.bcvsolutions.idm.acc.event.processor.provisioning.PrepareConnectorObjectProcessor;
 import eu.bcvsolutions.idm.acc.exception.ProvisioningException;
 import eu.bcvsolutions.idm.acc.service.api.ProvisioningService;
-import eu.bcvsolutions.idm.acc.service.api.SysProvisioningArchiveService;
 import eu.bcvsolutions.idm.acc.service.api.SysProvisioningAttributeService;
 import eu.bcvsolutions.idm.acc.service.api.SysProvisioningOperationService;
 import eu.bcvsolutions.idm.acc.service.api.SysSchemaAttributeService;
 import eu.bcvsolutions.idm.acc.service.api.SysSchemaObjectClassService;
 import eu.bcvsolutions.idm.acc.service.api.SysSystemAttributeMappingService;
-import eu.bcvsolutions.idm.acc.service.api.SysSystemEntityService;
 import eu.bcvsolutions.idm.acc.service.api.SysSystemMappingService;
 import eu.bcvsolutions.idm.acc.service.api.SysSystemService;
 import eu.bcvsolutions.idm.core.api.event.AbstractEntityEventProcessor;
 import eu.bcvsolutions.idm.core.api.event.DefaultEventResult;
 import eu.bcvsolutions.idm.core.api.event.EntityEvent;
 import eu.bcvsolutions.idm.core.api.event.EventResult;
-import eu.bcvsolutions.idm.core.api.service.ConfidentialStorage;
-import eu.bcvsolutions.idm.core.api.service.IdmPasswordPolicyService;
-import eu.bcvsolutions.idm.core.api.service.LookupService;
-import eu.bcvsolutions.idm.core.notification.api.service.NotificationManager;
 import eu.bcvsolutions.idm.core.security.api.domain.Enabled;
 import eu.bcvsolutions.idm.extras.config.domain.ExtrasConfiguration;
 import eu.bcvsolutions.idm.ic.api.IcAttribute;
@@ -56,9 +57,9 @@ import eu.bcvsolutions.idm.ic.api.IcConnectorObject;
 import eu.bcvsolutions.idm.ic.api.IcObjectClass;
 import eu.bcvsolutions.idm.ic.api.IcUidAttribute;
 import eu.bcvsolutions.idm.ic.filter.api.IcFilter;
-import eu.bcvsolutions.idm.ic.filter.api.IcResultsHandler;
 import eu.bcvsolutions.idm.ic.filter.impl.IcEqualsFilter;
 import eu.bcvsolutions.idm.ic.impl.IcAttributeImpl;
+import eu.bcvsolutions.idm.ic.impl.IcConnectorObjectImpl;
 import eu.bcvsolutions.idm.ic.impl.IcObjectClassImpl;
 import eu.bcvsolutions.idm.ic.impl.IcUidAttributeImpl;
 import eu.bcvsolutions.idm.ic.service.api.IcConnectorFacade;
@@ -77,8 +78,6 @@ public class ExtrasCrossAdGroupsProvisioningProcessor extends AbstractEntityEven
 	private static final org.slf4j.Logger LOG = org.slf4j.LoggerFactory
 			.getLogger(PrepareConnectorObjectProcessor.class);
 
-	private final String MEMBER_ATTRIBUTE = "member";
-
 	private final SysSystemMappingService systemMappingService;
 	private final SysSystemAttributeMappingService attributeMappingService;
 	private final IcConnectorFacade connectorFacade;
@@ -86,16 +85,9 @@ public class ExtrasCrossAdGroupsProvisioningProcessor extends AbstractEntityEven
 	private final SysProvisioningOperationService provisioningOperationService;
 	private final SysSchemaAttributeService schemaAttributeService;
 	private final SysSchemaObjectClassService schemaObjectClassService;
-	private final ProvisioningConfiguration provisioningConfiguration;
 
 	@Autowired
 	private ProvisioningService provisioningService;
-	@Autowired
-	private LookupService lookupService;
-	@Autowired
-	private IdmPasswordPolicyService passwordPolicyService;
-	@Autowired
-	private ConfidentialStorage confidentialStorage;
 	@Autowired
 	private SysProvisioningAttributeService provisioningAttributeService;
 	@Autowired
@@ -105,28 +97,20 @@ public class ExtrasCrossAdGroupsProvisioningProcessor extends AbstractEntityEven
 	public ExtrasCrossAdGroupsProvisioningProcessor(
 			IcConnectorFacade connectorFacade,
 			SysSystemService systemService,
-			SysSystemEntityService systemEntityService,
-			NotificationManager notificationManager, // @deprecated @since 9.2.2
 			SysProvisioningOperationService provisioningOperationService,
 			SysSystemMappingService systemMappingService,
 			SysSystemAttributeMappingService attributeMappingService,
 			SysSchemaAttributeService schemaAttributeService,
-			SysProvisioningArchiveService provisioningArchiveService,
-			SysSchemaObjectClassService schemaObjectClassService,
-			ProvisioningConfiguration provisioningConfiguration) {
+			SysSchemaObjectClassService schemaObjectClassService) {
 		super(ProvisioningEventType.CREATE, ProvisioningEventType.UPDATE);
 		//
-		Assert.notNull(systemEntityService);
 		Assert.notNull(systemMappingService);
 		Assert.notNull(attributeMappingService);
 		Assert.notNull(connectorFacade);
 		Assert.notNull(systemService);
-		Assert.notNull(notificationManager);
 		Assert.notNull(provisioningOperationService);
 		Assert.notNull(schemaAttributeService);
-		Assert.notNull(provisioningArchiveService);
 		Assert.notNull(schemaObjectClassService);
-		Assert.notNull(provisioningConfiguration);
 		//
 		this.systemMappingService = systemMappingService;
 		this.attributeMappingService = attributeMappingService;
@@ -135,7 +119,6 @@ public class ExtrasCrossAdGroupsProvisioningProcessor extends AbstractEntityEven
 		this.provisioningOperationService = provisioningOperationService;
 		this.schemaAttributeService = schemaAttributeService;
 		this.schemaObjectClassService = schemaObjectClassService;
-		this.provisioningConfiguration = provisioningConfiguration;
 	}
 
 	@Override
@@ -158,7 +141,7 @@ public class ExtrasCrossAdGroupsProvisioningProcessor extends AbstractEntityEven
 			return new DefaultEventResult<>(event, this);
 		}
 
-		String userDn = "";
+		String userDn;
 
 		// load old DN, in event we have the new one but we need the old one for finding users groups
 		IcObjectClass objectClass = provisioningOperation.getProvisioningContext().getConnectorObject()
@@ -172,11 +155,11 @@ public class ExtrasCrossAdGroupsProvisioningProcessor extends AbstractEntityEven
 			throw new ProvisioningException(AccResultCode.CONNECTOR_CONFIGURATION_FOR_SYSTEM_NOT_FOUND,
 					ImmutableMap.of("system", system.getName()));
 		}
+		IcConnectorObject existsConnectorObject;
 		try {
-			IcConnectorObject existsConnectorObject;
-				IcUidAttribute uidAttribute = new IcUidAttributeImpl(null, uid, null);
-				existsConnectorObject = connectorFacade.readObject(system.getConnectorInstance(), connectorConfig,
-						objectClass, uidAttribute);
+			IcUidAttribute uidAttribute = new IcUidAttributeImpl(null, uid, null);
+			existsConnectorObject = connectorFacade.readObject(system.getConnectorInstance(), connectorConfig,
+					objectClass, uidAttribute);
 			if (existsConnectorObject == null) {
 				return new DefaultEventResult<>(event, this);
 			} else {
@@ -192,14 +175,14 @@ public class ExtrasCrossAdGroupsProvisioningProcessor extends AbstractEntityEven
 
 		Set<String> userGroups = new HashSet<>();
 
-		// find user on system
+		// find all user's groups
 		String finalUserDn = userDn;
 		adSystems.forEach(adSystem -> {
 			SysSystemDto adSystemDto = systemService.get(adSystem);
-			userGroups.addAll(findUserOnSystem(adSystemDto, groupClass, finalUserDn));
+			userGroups.addAll(findUserGroupsOnSystem(adSystemDto, groupClass, finalUserDn));
 		});
 
-		// check if user has more roles then in IdM = that can happen if he has roles from other servers then the normal search will not return all
+		// check if user has more roles then in IdM = that can happen if he has roles from other servers then the normal search will not return all of them
 		Map<ProvisioningAttributeDto, Object> fullAccountObject = provisioningOperationService
 				.getFullAccountObject(provisioningOperation);
 
@@ -214,41 +197,89 @@ public class ExtrasCrossAdGroupsProvisioningProcessor extends AbstractEntityEven
 		ProvisioningAttributeDto provisioningAttribute = ldapGroups.get().getKey();
 		Object idmValue = fullAccountObject.get(provisioningAttribute);
 
-		// run some similar code for merge attribute as in prepere connector processor
+		// run same code for merge attribute as in prepare connector processor
 		Object resultMerged = resolveMergeValues(provisioningAttribute, idmValue,
 				new ArrayList<>(userGroups), provisioningOperation);
 
-		// TODO set new merged value back to provisioning
-//		IcConnectorObject updateConnectorObject;
-//		if (provisioningContext.getAccountObject() == null) {
-//			updateConnectorObject = connectorObject;
-//		} else {
-//			updateConnectorObject = new IcConnectorObjectImpl(systemEntityUid, objectClass, null);
-		// Or this attribute must be send every time (event if was not changed)
-//		return attributeMappingService.createIcAttribute(schemaAttribute, idmValue);
-//		 I'll get updatedAttribute fronm the line above
-//		if (updatedAttribute != null) {
-//			updateConnectorObject.getAttributes().add(updatedAttribute);
-//		}
-//		provisioningOperation.getProvisioningContext().setConnectorObject(updateConnectorObject);
+		IcConnectorObject updateConnectorObject;
 
-		IcConnectorObject connectorObject = provisioningOperation.getProvisioningContext().getConnectorObject();
-		IcAttribute memberAttr = connectorObject.getAttributeByName(MEMBER_ATTRIBUTE);
-		if (memberAttr == null) {
-			memberAttr = new IcAttributeImpl(MEMBER_ATTRIBUTE, new ArrayList<>());
+		SysSystemMappingDto mapping = getMapping(system, provisioningOperation.getEntityType());
+		SysSchemaObjectClassDto schemaObjectClassDto = schemaObjectClassService.get(mapping.getObjectClass());
+		List<SysSchemaAttributeDto> schemaAttributes = findSchemaAttributes(system, schemaObjectClassDto);
+		Optional<SysSchemaAttributeDto> schemaAttributeOptional = schemaAttributes //
+				.stream() //
+				.filter(schemaAttribute -> { //
+					return provisioningAttribute.getSchemaAttributeName().equals(schemaAttribute.getName());
+				}) //
+				.findFirst();
+
+		if (!schemaAttributeOptional.isPresent()) {
+			throw new ProvisioningException(AccResultCode.PROVISIONING_SCHEMA_ATTRIBUTE_IS_FOUND,
+					ImmutableMap.of("attribute", provisioningAttribute.getSchemaAttributeName()));
 		}
 
+		SysSchemaAttributeDto schemaAttribute = schemaAttributeOptional.get();
+
+		// if roles are not same
+		if (!provisioningService.isAttributeValueEquals(resultMerged, new ArrayList<>(userGroups), schemaAttribute)) {
+			List<IcAttribute> attributesWithoutGroups = getIcAttributes(provisioningOperation, userGroups, idmValue, resultMerged, schemaAttribute);
+			updateConnectorObject = new IcConnectorObjectImpl(uid, objectClass, attributesWithoutGroups);
+		} else {
+			// remove ldapGroups from provisioning operation
+			IcConnectorObject connectorObject = provisioningOperation.getProvisioningContext().getConnectorObject();
+			List<IcAttribute> attributes = connectorObject.getAttributes();
+			List<IcAttribute> attributesWithoutGroups = attributes.stream()
+					.filter(icAttribute -> !icAttribute.getName().equals("ldapGroups"))
+					.collect(Collectors.toList());
+			updateConnectorObject = new IcConnectorObjectImpl(uid, objectClass, attributesWithoutGroups);
+		}
+		provisioningOperation.getProvisioningContext().setConnectorObject(updateConnectorObject);
 		// set back to event content
 		provisioningOperation = provisioningOperationService.saveOperation(provisioningOperation);
 		// log attributes used in provisioning context into provisioning attributes
 		provisioningAttributeService.saveAttributes(provisioningOperation);
 		//
 		event.setContent(provisioningOperation);
-
 		return new DefaultEventResult<>(event, this);
 	}
 
-	private Set<String> findUserOnSystem(SysSystemDto system, IcObjectClass objectClass, String dn) {
+	private List<IcAttribute> getIcAttributes(SysProvisioningOperationDto provisioningOperation, Set<String> userGroups, Object idmValue, Object resultMerged, SysSchemaAttributeDto schemaAttribute) {
+		// set correct value to ldapGroups attribute
+		IcAttribute updatedAttribute = attributeMappingService.createIcAttribute(schemaAttribute, resultMerged);
+
+		IcConnectorObject connectorObject = provisioningOperation.getProvisioningContext().getConnectorObject();
+		List<IcAttribute> attributes = connectorObject.getAttributes();
+		List<IcAttribute> attributesWithoutGroups = attributes.stream()
+				.filter(icAttribute -> !icAttribute.getName().equals("ldapGroups"))
+				.collect(Collectors.toList());
+		attributesWithoutGroups.add(updatedAttribute);
+
+		// prepare two list, roles to add and roles to remove so I dont need to search it again in connector
+		List<Object> idmValues = new ArrayList<>();
+		if (idmValue instanceof List) {
+			idmValues.addAll((List<?>) idmValue);
+		} else {
+			if (idmValue != null) {
+				idmValues.add(String.valueOf(idmValue));
+			}
+		}
+		List<Object> systemValues = new ArrayList<>(userGroups);
+
+		List<Object> toAdd = new ArrayList<>(idmValues);
+		List<Object> toRemove = new ArrayList<>(systemValues);
+
+		toAdd.removeAll(systemValues);
+		toRemove.removeAll(idmValues);
+
+		IcAttribute rolesToAdd = new IcAttributeImpl("ldapGroupsToAdd", toAdd, true);
+		IcAttribute rolesToRemove = new IcAttributeImpl("ldapGroupsToRemove", toRemove, true);
+
+		attributesWithoutGroups.add(rolesToAdd);
+		attributesWithoutGroups.add(rolesToRemove);
+		return attributesWithoutGroups;
+	}
+
+	private Set<String> findUserGroupsOnSystem(SysSystemDto system, IcObjectClass objectClass, String dn) {
 		Set<String> result = new HashSet<>();
 		// Find connector identification persisted in system
 		if (system.getConnectorKey() == null) {
@@ -263,18 +294,14 @@ public class ExtrasCrossAdGroupsProvisioningProcessor extends AbstractEntityEven
 		}
 		//
 		try {
-			//Collections.singletonList("CN=Roman Kuceratest,CN=Users,DC=piskoviste,DC=bcv")
-			IcAttribute membersAttribute = new IcAttributeImpl(MEMBER_ATTRIBUTE, dn);
+			IcAttribute membersAttribute = new IcAttributeImpl("member", dn);
 			IcFilter filter = new IcEqualsFilter(membersAttribute);
-			connectorFacade.search(system.getConnectorInstance(), connectorConfig, objectClass, filter, new IcResultsHandler() {
-				@Override
-				public boolean handle(IcConnectorObject connectorObject) {
-					IcAttribute groupDn = connectorObject.getAttributeByName("__NAME__");
-					if (groupDn != null && !StringUtils.isEmpty(groupDn.getValue())) {
-						result.add(String.valueOf(groupDn.getValue()));
-					}
-					return true;
+			connectorFacade.search(system.getConnectorInstance(), connectorConfig, objectClass, filter, connectorObject -> {
+				IcAttribute groupDn = connectorObject.getAttributeByName("__NAME__");
+				if (groupDn != null && !StringUtils.isEmpty(groupDn.getValue())) {
+					result.add(String.valueOf(groupDn.getValue()));
 				}
+				return true;
 			});
 		} catch (Exception ex) {
 			LOG.info("Error during getting user", ex);
@@ -305,19 +332,19 @@ public class ExtrasCrossAdGroupsProvisioningProcessor extends AbstractEntityEven
 		List<Object> connectorValues = Lists.newArrayList();
 		List<Object> idmValues = Lists.newArrayList();
 
-		if(connectorValue instanceof List) {
+		if (connectorValue instanceof List) {
 			resultValues.addAll((List<?>) connectorValue);
 			connectorValues.addAll((List<?>) connectorValue);
-		}else {
-			if(connectorValue != null) {
+		} else {
+			if (connectorValue != null) {
 				resultValues.add(connectorValue);
 				connectorValues.add(connectorValue);
 			}
 		}
-		if(idmValues instanceof List) {
+		if (idmValues instanceof List) {
 			idmValues.addAll((List<?>) idmValue);
-		}else {
-			if(idmValue != null) {
+		} else {
+			if (idmValue != null) {
 				idmValues.add(idmValue);
 			}
 		}
@@ -330,15 +357,15 @@ public class ExtrasCrossAdGroupsProvisioningProcessor extends AbstractEntityEven
 
 		// Controlled value can be Collection, we need to create flat list for all values
 		controlledValues.forEach(controlledValue -> {
-			if(controlledValue instanceof Collection) {
+			if (controlledValue instanceof Collection) {
 				controlledValuesFlat.addAll((Collection<? extends Serializable>) controlledValue);
-			}else {
+			} else {
 				controlledValuesFlat.add(controlledValue);
 			}
 		});
 
 		// Merge IdM values with connector values
-		idmValues.stream().forEach(value -> {
+		idmValues.forEach(value -> {
 			if (!connectorValues.contains(value)) {
 				resultValues.add(value);
 			}
@@ -350,10 +377,7 @@ public class ExtrasCrossAdGroupsProvisioningProcessor extends AbstractEntityEven
 			List<?> deletedValues = controlledValuesFlat //
 					.stream() //
 					.filter(controlledValue -> { //
-						if (idmValues.contains(controlledValue)) {
-							return false;
-						}
-						return true;
+						return !idmValues.contains(controlledValue);
 					}).collect(Collectors.toList());
 			// Remove all deleted values (managed by IdM)
 			resultValues.removeAll(deletedValues);
@@ -361,4 +385,36 @@ public class ExtrasCrossAdGroupsProvisioningProcessor extends AbstractEntityEven
 
 		return resultValues;
 	}
+
+	private SysSystemMappingDto getMapping(SysSystemDto system, SystemEntityType entityType) {
+		List<SysSystemMappingDto> systemMappings = systemMappingService.findBySystem(system,
+				SystemOperationType.PROVISIONING, entityType);
+		if (systemMappings == null || systemMappings.isEmpty()) {
+			throw new IllegalStateException(MessageFormat.format(
+					"System [{0}] does not have mapping, provisioning will not be executed. Add some mapping for entity type [{1}]",
+					system.getName(), entityType));
+		}
+		if (systemMappings.size() != 1) {
+			throw new IllegalStateException(MessageFormat.format(
+					"System [{0}] is wrong configured! Remove duplicit mapping for entity type [{1}]", system.getName(),
+					entityType));
+		}
+		return systemMappings.get(0);
+	}
+
+	/**
+	 * Find list of {@link SysSchemaAttribute} by system and objectClass
+	 *
+	 * @param objectClass
+	 * @param system
+	 * @return
+	 */
+	private List<SysSchemaAttributeDto> findSchemaAttributes(SysSystemDto system, SysSchemaObjectClassDto objectClass) {
+
+		SysSchemaAttributeFilter schemaAttributeFilter = new SysSchemaAttributeFilter();
+		schemaAttributeFilter.setSystemId(system.getId());
+		schemaAttributeFilter.setObjectClassId(objectClass.getId());
+		return schemaAttributeService.find(schemaAttributeFilter, null).getContent();
+	}
+
 }
