dfaced6a3787e77fc5daab84dbe83fa134faa808
klementm <marek.klement@bcvsolutions.eu>
2020-03-03 14:18:32 +0100

    [#2092] test is working now - fixed test order and created other testing scope

--- Realization/backend/idm-extras/src/test/java/eu/bcvsolutions/idm/extras/event/processor/role/RoleRequestIdentitySystemProcessorTest.java
+++ Realization/backend/idm-extras/src/test/java/eu/bcvsolutions/idm/extras/event/processor/role/RoleRequestIdentitySystemProcessorTest.java
@@ -1,9 +1,8 @@
 package eu.bcvsolutions.idm.extras.event.processor.role;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.util.List;
-import java.util.stream.Stream;
 
 import org.junit.After;
 import org.junit.Before;
@@ -13,10 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import eu.bcvsolutions.idm.InitApplicationData;
 import eu.bcvsolutions.idm.acc.domain.SystemEntityType;
 import eu.bcvsolutions.idm.acc.domain.SystemOperationType;
-import eu.bcvsolutions.idm.acc.dto.AccIdentityAccountDto;
 import eu.bcvsolutions.idm.acc.dto.SysProvisioningArchiveDto;
-import eu.bcvsolutions.idm.acc.dto.SysProvisioningOperationDto;
-import eu.bcvsolutions.idm.acc.dto.SysRoleSystemDto;
 import eu.bcvsolutions.idm.acc.dto.SysSchemaAttributeDto;
 import eu.bcvsolutions.idm.acc.dto.SysSchemaObjectClassDto;
 import eu.bcvsolutions.idm.acc.dto.SysSystemAttributeMappingDto;
@@ -24,21 +20,15 @@ import eu.bcvsolutions.idm.acc.dto.SysSystemDto;
 import eu.bcvsolutions.idm.acc.dto.SysSystemMappingDto;
 import eu.bcvsolutions.idm.acc.dto.filter.SysProvisioningOperationFilter;
 import eu.bcvsolutions.idm.acc.dto.filter.SysSchemaAttributeFilter;
-import eu.bcvsolutions.idm.acc.service.api.ProvisioningService;
 import eu.bcvsolutions.idm.acc.service.api.SysProvisioningArchiveService;
-import eu.bcvsolutions.idm.acc.service.api.SysProvisioningOperationService;
 import eu.bcvsolutions.idm.acc.service.api.SysSchemaAttributeService;
-import eu.bcvsolutions.idm.acc.service.api.SysSchemaObjectClassService;
 import eu.bcvsolutions.idm.acc.service.api.SysSystemAttributeMappingService;
 import eu.bcvsolutions.idm.acc.service.api.SysSystemMappingService;
 import eu.bcvsolutions.idm.acc.service.api.SysSystemService;
-import eu.bcvsolutions.idm.core.api.domain.OperationState;
 import eu.bcvsolutions.idm.core.api.dto.IdmIdentityDto;
-import eu.bcvsolutions.idm.core.api.dto.IdmIdentityRoleDto;
 import eu.bcvsolutions.idm.core.api.dto.IdmRoleDto;
 import eu.bcvsolutions.idm.core.api.dto.IdmRoleRequestDto;
 import eu.bcvsolutions.idm.core.api.service.ConfigurationService;
-import eu.bcvsolutions.idm.core.api.service.IdmIdentityRoleService;
 import eu.bcvsolutions.idm.core.api.service.IdmRoleRequestService;
 import eu.bcvsolutions.idm.extras.TestHelper;
 import eu.bcvsolutions.idm.extras.TestResource;
@@ -55,23 +45,15 @@ public class RoleRequestIdentitySystemProcessorTest extends AbstractIntegrationT
 	@Autowired
 	private ExtrasConfiguration extrasConfiguration;
 	@Autowired
-	private SysProvisioningOperationService provisioningOperationService;
-	@Autowired
 	private SysSystemService systemService;
 	@Autowired
 	private SysSystemMappingService mappingService;
-	@Autowired
-	private SysSchemaObjectClassService objectClassService;
 	@Autowired
 	private SysSchemaAttributeService schemaAttributeService;
 	@Autowired
 	private SysSystemAttributeMappingService attributeMappingService;
 	@Autowired
 	private IdmRoleRequestService roleRequestService;
-	@Autowired
-	private IdmIdentityRoleService identityRoleService;
-	@Autowired
-	private ProvisioningService provisioningService;
 	@Autowired
 	private SysProvisioningArchiveService sysProvisioningArchiveService;
 
@@ -86,13 +68,13 @@ public class RoleRequestIdentitySystemProcessorTest extends AbstractIntegrationT
 	}
 
 	@Test
-	public void testCreateSystemAndProvideRole(){
+	public void testCreateSystemAndProvideRole() {
 		// create system
 		SysSystemDto system = testHelper.createSystem(TestResource.TABLE_NAME);
 		// generate schema
 		List<SysSchemaObjectClassDto> objectClasses = systemService.generateSchema(system);
 		//
-		assertEquals(1,objectClasses.size());
+		assertEquals(1, objectClasses.size());
 		// create mapping with one attribute mapped
 		createMappingSystem(SystemEntityType.IDENTITY,
 				objectClasses.get(0), "TestMappingProvisioning");
@@ -102,24 +84,36 @@ public class RoleRequestIdentitySystemProcessorTest extends AbstractIntegrationT
 		IdmRoleDto systemRole = testHelper.createRole();
 		// put role on the system
 		testHelper.createRoleSystem(systemRole, system);
-		// set configuration property
-		configurationService.setValue(extrasConfiguration.EXTRAS_SYSTEM_EXCHANGE_ID, system.getId().toString());
 		// create identity
 		IdmIdentityDto identity = testHelper.createIdentity();
 		// give identity system role
-		IdmRoleRequestDto roleRequest2 = testHelper.createRoleRequest(identity, systemRole);
-		IdmRoleRequestDto idmRoleRequest = roleRequestService.startRequest(roleRequest2.getId(), true);
-		roleRequestService.executeRequest(idmRoleRequest.getId());
+		IdmRoleRequestDto roleRequestA = testHelper.createRoleRequest(identity, systemRole);
+		roleRequestA = roleRequestService.startRequest(roleRequestA.getId(), true);
+		roleRequestService.executeRequest(roleRequestA.getId());
 
-		// give her just other role - should start the processor
+		// give identity another role and test if not provided
 		IdmRoleRequestDto roleRequest = testHelper.createRoleRequest(identity, role);
-		IdmRoleRequestDto idmRoleRequestDto = roleRequestService.startRequest(roleRequest.getId(), true);
-		roleRequestService.executeRequest(idmRoleRequestDto.getId());
-		// test result
+		roleRequest = roleRequestService.startRequest(roleRequest.getId(), true);
+		roleRequestService.executeRequest(roleRequest.getId());
+		//
 		SysProvisioningOperationFilter filter = new SysProvisioningOperationFilter();
 		filter.setSystemId(system.getId());
 		List<SysProvisioningArchiveDto> content = sysProvisioningArchiveService.find(filter, null).getContent();
-		assertEquals(2,content.size());
+		// test just created
+		assertEquals(1, content.size());
+		// start processor
+		configurationService.setValue("idm.sec.extras.processor.extras-role-request-identity-system-processor.enabled"
+				, "true");
+		configurationService.setValue(extrasConfiguration.EXTRAS_SYSTEM_EXCHANGE_ID, system.getId().toString());
+		// new role request should start processor
+		IdmRoleDto newRole = testHelper.createRole();
+		IdmRoleRequestDto roleRequestNew = testHelper.createRoleRequest(identity, newRole);
+		roleRequestNew = roleRequestService.startRequest(roleRequestNew.getId(), true);
+		roleRequestService.executeRequest(roleRequestNew.getId());
+		// test result
+		content = sysProvisioningArchiveService.find(filter, null).getContent();
+		// should be create and one update after setting a processor
+		assertEquals(2, content.size());
 	}
 
 	public SysSystemMappingDto createMappingSystem(SystemEntityType type, SysSchemaObjectClassDto objectClass,
@@ -139,7 +133,7 @@ public class RoleRequestIdentitySystemProcessorTest extends AbstractIntegrationT
 				"__NAME__")).toArray();
 		//
 		SysSystemAttributeMappingDto uid = new SysSystemAttributeMappingDto();
-		uid.setSchemaAttribute(((SysSchemaAttributeDto)(newRes[0])).getId());
+		uid.setSchemaAttribute(((SysSchemaAttributeDto) (newRes[0])).getId());
 		uid.setEntityAttribute(true);
 		uid.setName("username");
 		uid.setIdmPropertyName("username");
