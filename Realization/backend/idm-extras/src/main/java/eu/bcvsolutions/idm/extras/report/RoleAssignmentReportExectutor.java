5300589172c2256f27410222b85b7a314ea81335
klementm <marek.klement@bcvsolutions.eu>
2020-04-08 18:02:31 +0200

    [#2192] removed unused imports

--- Realization/backend/idm-extras/src/main/java/eu/bcvsolutions/idm/extras/report/RoleAssignmentReportExectutor.java
+++ Realization/backend/idm-extras/src/main/java/eu/bcvsolutions/idm/extras/report/RoleAssignmentReportExectutor.java
@@ -10,23 +10,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-import eu.bcvsolutions.idm.acc.dto.SysRoleSystemDto;
-import eu.bcvsolutions.idm.acc.dto.filter.SysRoleSystemFilter;
-import eu.bcvsolutions.idm.acc.service.api.SysRoleSystemService;
-import eu.bcvsolutions.idm.acc.service.api.SysSystemService;
-import eu.bcvsolutions.idm.core.api.audit.dto.IdmAuditDto;
-import eu.bcvsolutions.idm.core.api.audit.dto.IdmAuditEntityDto;
-import eu.bcvsolutions.idm.core.api.audit.dto.filter.IdmAuditFilter;
-import eu.bcvsolutions.idm.core.api.audit.service.IdmAuditService;
-import eu.bcvsolutions.idm.core.api.dto.BaseDto;
-import eu.bcvsolutions.idm.core.api.dto.IdmIdentityContractDto;
-import eu.bcvsolutions.idm.core.api.dto.IdmRoleDto;
-import eu.bcvsolutions.idm.core.api.service.IdmIdentityContractService;
-import eu.bcvsolutions.idm.core.api.service.IdmIdentityRoleService;
-import eu.bcvsolutions.idm.core.api.service.IdmRoleService;
-import eu.bcvsolutions.idm.core.model.entity.IdmIdentityRole;
-import eu.bcvsolutions.idm.core.model.entity.IdmIdentityRole_;
-import eu.bcvsolutions.idm.extras.ExtrasModuleDescriptor;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,158 +25,173 @@ import com.fasterxml.jackson.core.JsonEncoding;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.google.common.collect.Lists;
 
+import eu.bcvsolutions.idm.acc.dto.SysRoleSystemDto;
+import eu.bcvsolutions.idm.acc.dto.filter.SysRoleSystemFilter;
+import eu.bcvsolutions.idm.acc.service.api.SysRoleSystemService;
+import eu.bcvsolutions.idm.acc.service.api.SysSystemService;
+import eu.bcvsolutions.idm.core.api.audit.dto.IdmAuditDto;
+import eu.bcvsolutions.idm.core.api.audit.dto.IdmAuditEntityDto;
+import eu.bcvsolutions.idm.core.api.audit.dto.filter.IdmAuditFilter;
+import eu.bcvsolutions.idm.core.api.audit.service.IdmAuditService;
+import eu.bcvsolutions.idm.core.api.dto.IdmIdentityContractDto;
 import eu.bcvsolutions.idm.core.api.dto.IdmIdentityDto;
-import eu.bcvsolutions.idm.core.api.dto.filter.IdmIdentityFilter;
+import eu.bcvsolutions.idm.core.api.dto.IdmRoleDto;
+import eu.bcvsolutions.idm.core.api.service.IdmIdentityContractService;
 import eu.bcvsolutions.idm.core.api.service.IdmIdentityService;
-import eu.bcvsolutions.idm.core.eav.api.domain.BaseFaceType;
-import eu.bcvsolutions.idm.core.eav.api.domain.PersistentType;
+import eu.bcvsolutions.idm.core.api.service.IdmRoleService;
 import eu.bcvsolutions.idm.core.eav.api.dto.IdmFormAttributeDto;
-import eu.bcvsolutions.idm.core.eav.api.dto.IdmFormInstanceDto;
 import eu.bcvsolutions.idm.core.ecm.api.dto.IdmAttachmentDto;
+import eu.bcvsolutions.idm.core.model.entity.IdmIdentityRole;
+import eu.bcvsolutions.idm.core.model.entity.IdmIdentityRole_;
 import eu.bcvsolutions.idm.core.model.entity.IdmIdentity_;
 import eu.bcvsolutions.idm.core.security.api.domain.Enabled;
-import eu.bcvsolutions.idm.core.security.api.domain.IdmBasePermission;
+import eu.bcvsolutions.idm.extras.ExtrasModuleDescriptor;
 import eu.bcvsolutions.idm.rpt.api.dto.RptReportDto;
 import eu.bcvsolutions.idm.rpt.api.exception.ReportGenerateException;
 import eu.bcvsolutions.idm.rpt.api.executor.AbstractReportExecutor;
 
-    /**
-     * Example report with identities
-     * - filter for enabled / disabled identities
-     * - uses json stream to save a lot of identities (+creates temporary file)
-     *
-     * @author Radek Tomiška
-     *
-     */
-    @Enabled(ExtrasModuleDescriptor.MODULE_ID)
-    @Component("extrasRoleAssignmentReportExectutor")
-    @Description("Identities - role assignments")
-    public class RoleAssignmentReportExectutor extends AbstractReportExecutor {
+/**
+ * Example report with identities
+ * - filter for enabled / disabled identities
+ * - uses json stream to save a lot of identities (+creates temporary file)
+ *
+ * @author Radek Tomiška
+ */
+@Enabled(ExtrasModuleDescriptor.MODULE_ID)
+@Component("extrasRoleAssignmentReportExectutor")
+@Description("Identities - role assignments")
+public class RoleAssignmentReportExectutor extends AbstractReportExecutor {
 
-        public static final String REPORT_NAME = "extras-role-assignments-report"; // report ~ executor name
-        //
+	public static final String REPORT_NAME = "extras-role-assignments-report"; // report ~ executor name
+	//
 
-        @Autowired private IdmAuditService idmAuditService;
-        @Autowired private IdmIdentityService identityService;
-        @Autowired private IdmRoleService roleService;
-        @Autowired private IdmIdentityContractService contractService;
-        @Autowired private SysRoleSystemService roleSystemService;
-        @Autowired private SysSystemService sysSystemService;
+	@Autowired
+	private IdmAuditService idmAuditService;
+	@Autowired
+	private IdmIdentityService identityService;
+	@Autowired
+	private IdmRoleService roleService;
+	@Autowired
+	private IdmIdentityContractService contractService;
+	@Autowired
+	private SysRoleSystemService roleSystemService;
+	@Autowired
+	private SysSystemService sysSystemService;
 
-        /**
-         * Report ~ executor name
-         */
-        @Override
-        public String getName() {
-            return REPORT_NAME;
-        }
+	/**
+	 * Report ~ executor name
+	 */
+	@Override
+	public String getName() {
+		return REPORT_NAME;
+	}
 
-        @Override
-        public List<IdmFormAttributeDto> getFormAttributes() {
-            return Lists.newArrayList();
-        }
+	@Override
+	public List<IdmFormAttributeDto> getFormAttributes() {
+		return Lists.newArrayList();
+	}
 
-        @Override
-        protected IdmAttachmentDto generateData(RptReportDto report) {
-            File temp = null;
-            FileOutputStream outputStream = null;
-            try {
-                // prepare temp file for json stream
-                temp = getAttachmentManager().createTempFile();
-                outputStream = new FileOutputStream(temp);
-                // write into json stream
-                JsonGenerator jGenerator = getMapper().getFactory().createGenerator(outputStream, JsonEncoding.UTF8);
-                try {
-                    // json will be array of identities
-                    jGenerator.writeStartArray();
-                    // initialize filter by given form - transform to multi value map
-                    // => form attribute defined above will be automaticaly mapped to identity filter
-                    IdmAuditFilter filter = new IdmAuditFilter();
-                    filter.setType(IdmIdentityRole.class.getName());
-                    filter.setWithVersion(true);
-                    // report extends long running task - show progress by count and counter lrt attributes
-                    counter = 0L;
-                    // find a first page of identities
-                    Pageable pageable = new PageRequest(0, 100, new Sort(Direction.ASC, IdmIdentity_.username.getName()));
-                    do {
-                        Page<IdmAuditDto> auditDtos = idmAuditService.find(filter, null);
-                        if (count == null) {
-                            // report extends long running task - show progress by count and counter lrt attributes
-                            count = auditDtos.getTotalElements();
-                        }
-                        boolean canContinue = true;
-                        for (Iterator<IdmAuditDto> i = auditDtos.iterator(); i.hasNext() && canContinue;) {
-                            // write single identity into json
-                            RoleAssignmentReportDto reportDto = getReportDto((IdmAuditEntityDto) i.next());
-                            getMapper().writeValue(jGenerator, reportDto);
-                            //
-                            // supports cancel report generating (report extends long running task)
-                            ++counter;
-                            canContinue = updateState();
-                        }
-                        // iterate while next page of auditDtos is available
-                        pageable = auditDtos.hasNext() && canContinue ? auditDtos.nextPageable() : null;
-                    } while (pageable != null);
-                    //
-                    // close array of identities
-                    jGenerator.writeEndArray();
-                } finally {
-                    // close json stream
-                    jGenerator.close();
-                }
-                // save create temp file with array of identities in json as attachment
-                return createAttachment(report, new FileInputStream(temp));
-            } catch (IOException ex) {
-                throw new ReportGenerateException(report.getName(), ex);
-            } finally {
-                IOUtils.closeQuietly(outputStream); // just for sure - jGenerator should close stream itself
-                FileUtils.deleteQuietly(temp);
-            }
-        }
-
-
-        private RoleAssignmentReportDto getReportDto(IdmAuditEntityDto next) {
-            Map<String, Object> embedded = next.getEntity();
-            IdmIdentityContractDto contractDto = getContract(embedded.get(IdmIdentityRole_.identityContract.getName()));
-            IdmIdentityDto identityDto;
-            if(contractDto==null){
-                identityDto = null;
-            } else {
-                identityDto = getIdentity(contractDto.getIdentity());
-            }
-            IdmRoleDto roleDto = getRole(embedded.get(IdmIdentityRole_.role.getName()));
-            String system = getSystem(roleDto);
+	@Override
+	protected IdmAttachmentDto generateData(RptReportDto report) {
+		File temp = null;
+		FileOutputStream outputStream = null;
+		try {
+			// prepare temp file for json stream
+			temp = getAttachmentManager().createTempFile();
+			outputStream = new FileOutputStream(temp);
+			// write into json stream
+			JsonGenerator jGenerator = getMapper().getFactory().createGenerator(outputStream, JsonEncoding.UTF8);
+			try {
+				// json will be array of identities
+				jGenerator.writeStartArray();
+				// initialize filter by given form - transform to multi value map
+				// => form attribute defined above will be automaticaly mapped to identity filter
+				IdmAuditFilter filter = new IdmAuditFilter();
+				filter.setType(IdmIdentityRole.class.getName());
+				filter.setWithVersion(true);
+				// report extends long running task - show progress by count and counter lrt attributes
+				counter = 0L;
+				// find a first page of identities
+				Pageable pageable = new PageRequest(0, 100, new Sort(Direction.ASC, IdmIdentity_.username.getName()));
+				do {
+					Page<IdmAuditDto> auditDtos = idmAuditService.find(filter, null);
+					if (count == null) {
+						// report extends long running task - show progress by count and counter lrt attributes
+						count = auditDtos.getTotalElements();
+					}
+					boolean canContinue = true;
+					for (Iterator<IdmAuditDto> i = auditDtos.iterator(); i.hasNext() && canContinue; ) {
+						// write single identity into json
+						RoleAssignmentReportDto reportDto = getReportDto((IdmAuditEntityDto) i.next());
+						getMapper().writeValue(jGenerator, reportDto);
+						//
+						// supports cancel report generating (report extends long running task)
+						++counter;
+						canContinue = updateState();
+					}
+					// iterate while next page of auditDtos is available
+					pageable = auditDtos.hasNext() && canContinue ? auditDtos.nextPageable() : null;
+				} while (pageable != null);
+				//
+				// close array of identities
+				jGenerator.writeEndArray();
+			} finally {
+				// close json stream
+				jGenerator.close();
+			}
+			// save create temp file with array of identities in json as attachment
+			return createAttachment(report, new FileInputStream(temp));
+		} catch (IOException ex) {
+			throw new ReportGenerateException(report.getName(), ex);
+		} finally {
+			IOUtils.closeQuietly(outputStream); // just for sure - jGenerator should close stream itself
+			FileUtils.deleteQuietly(temp);
+		}
+	}
 
 
-            return new RoleAssignmentReportDto(identityDto, contractDto, roleDto, next.getRevisionDate(),
-                    next.getModification(), next.getModifier(), system);
-        }
+	private RoleAssignmentReportDto getReportDto(IdmAuditEntityDto next) {
+		Map<String, Object> embedded = next.getEntity();
+		IdmIdentityContractDto contractDto = getContract(embedded.get(IdmIdentityRole_.identityContract.getName()));
+		IdmIdentityDto identityDto;
+		if (contractDto == null) {
+			identityDto = null;
+		} else {
+			identityDto = getIdentity(contractDto.getIdentity());
+		}
+		IdmRoleDto roleDto = getRole(embedded.get(IdmIdentityRole_.role.getName()));
+		String system = getSystem(roleDto);
 
-        private String getSystem(IdmRoleDto roleDto) {
-            if(roleDto==null){
-                return "Role have been deleted - no system info!";
-            }
-            SysRoleSystemFilter filter = new SysRoleSystemFilter();
-            filter.setRoleId(roleDto.getId());
-            List<SysRoleSystemDto> systems = roleSystemService.find(filter, null, null).getContent();
-            return systems.stream()
-                    .map(s -> s.getSystem())
-                    .map(s -> sysSystemService.get(s))
-                    .map(s -> s.getCode())
-                    .reduce((s1, s2) -> s1 + "," + s2)
-                    .orElse("");
-        }
 
-        private IdmRoleDto getRole(Object o) {
-            return roleService.get((UUID)o, null);
-        }
+		return new RoleAssignmentReportDto(identityDto, contractDto, roleDto, next.getRevisionDate(),
+				next.getModification(), next.getModifier(), system);
+	}
 
-        private IdmIdentityDto getIdentity(UUID identity) {
-            return identityService.get(identity, null);
-        }
+	private String getSystem(IdmRoleDto roleDto) {
+		if (roleDto == null) {
+			return "Role have been deleted - no system info!";
+		}
+		SysRoleSystemFilter filter = new SysRoleSystemFilter();
+		filter.setRoleId(roleDto.getId());
+		List<SysRoleSystemDto> systems = roleSystemService.find(filter, null, null).getContent();
+		return systems.stream()
+				.map(s -> s.getSystem())
+				.map(s -> sysSystemService.get(s))
+				.map(s -> s.getCode())
+				.reduce((s1, s2) -> s1 + "," + s2)
+				.orElse("");
+	}
 
-        private IdmIdentityContractDto getContract(Object contractId) {
-            return contractService.get((UUID)contractId);
-        }
+	private IdmRoleDto getRole(Object o) {
+		return roleService.get((UUID) o, null);
+	}
 
-    }
+	private IdmIdentityDto getIdentity(UUID identity) {
+		return identityService.get(identity, null);
+	}
+
+	private IdmIdentityContractDto getContract(Object contractId) {
+		return contractService.get((UUID) contractId);
+	}
+
+}
--- Realization/backend/idm-extras/src/test/java/eu/bcvsolutions/idm/extras/report/RoleAssignmentReportIntegrationTest.java
+++ Realization/backend/idm-extras/src/test/java/eu/bcvsolutions/idm/extras/report/RoleAssignmentReportIntegrationTest.java
@@ -1,25 +1,10 @@
 package eu.bcvsolutions.idm.extras.report;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import eu.bcvsolutions.idm.InitTestData;
-import eu.bcvsolutions.idm.core.api.dto.IdmIdentityContractDto;
-import eu.bcvsolutions.idm.core.api.dto.IdmIdentityDto;
-import eu.bcvsolutions.idm.core.api.dto.IdmRoleDto;
-import eu.bcvsolutions.idm.core.api.dto.IdmRoleRequestDto;
-import eu.bcvsolutions.idm.core.api.dto.filter.IdmIdentityFilter;
-import eu.bcvsolutions.idm.core.api.service.IdmIdentityService;
-import eu.bcvsolutions.idm.core.eav.api.dto.IdmFormDefinitionDto;
-import eu.bcvsolutions.idm.core.eav.api.dto.IdmFormDto;
-import eu.bcvsolutions.idm.core.eav.api.dto.IdmFormValueDto;
-import eu.bcvsolutions.idm.core.ecm.api.service.AttachmentManager;
-import eu.bcvsolutions.idm.core.security.api.domain.GuardedString;
-import eu.bcvsolutions.idm.core.security.api.dto.LoginDto;
-import eu.bcvsolutions.idm.core.security.api.service.LoginService;
-import eu.bcvsolutions.idm.extras.ExtrasModuleDescriptor;
-import eu.bcvsolutions.idm.rpt.api.dto.RptReportDto;
-import eu.bcvsolutions.idm.test.api.AbstractIntegrationTest;
-import eu.bcvsolutions.idm.test.api.TestHelper;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.UUID;
+
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -28,105 +13,183 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.List;
-import java.util.UUID;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import eu.bcvsolutions.idm.InitTestData;
+import eu.bcvsolutions.idm.core.api.dto.IdmIdentityContractDto;
+import eu.bcvsolutions.idm.core.api.dto.IdmIdentityDto;
+import eu.bcvsolutions.idm.core.api.dto.IdmIdentityRoleDto;
+import eu.bcvsolutions.idm.core.api.dto.IdmRoleDto;
+import eu.bcvsolutions.idm.core.api.dto.IdmRoleRequestDto;
+import eu.bcvsolutions.idm.core.api.service.IdmConceptRoleRequestService;
+import eu.bcvsolutions.idm.core.api.service.IdmIdentityRoleService;
+import eu.bcvsolutions.idm.core.api.service.IdmIdentityService;
+import eu.bcvsolutions.idm.core.api.service.IdmRoleRequestService;
+import eu.bcvsolutions.idm.core.api.service.IdmRoleService;
+import eu.bcvsolutions.idm.core.ecm.api.service.AttachmentManager;
+import eu.bcvsolutions.idm.core.security.api.domain.GuardedString;
+import eu.bcvsolutions.idm.core.security.api.dto.LoginDto;
+import eu.bcvsolutions.idm.core.security.api.service.LoginService;
+import eu.bcvsolutions.idm.extras.ExtrasModuleDescriptor;
+import eu.bcvsolutions.idm.rpt.api.dto.RptReportDto;
+import eu.bcvsolutions.idm.test.api.AbstractIntegrationTest;
+import eu.bcvsolutions.idm.test.api.TestHelper;
 
 /**
  * Created by peter on 24.7.19.
  */
 public class RoleAssignmentReportIntegrationTest extends AbstractIntegrationTest {
 
-    @Autowired
-    private TestHelper helper;
-    @Autowired
-    private RoleAssignmentReportExectutor reportExecutor;
-    @Autowired
-    private IdmIdentityService identityService;
-    @Autowired
-    private AttachmentManager attachmentManager;
-    @Qualifier("objectMapper")
-    @Autowired
-    private ObjectMapper mapper;
-    @Autowired
-    private LoginService loginService;
-    @Autowired
-    private RoleAssignmentReportRenderer xlsxRenderer;
+	@Autowired
+	private TestHelper helper;
+	@Autowired
+	private RoleAssignmentReportExectutor reportExecutor;
+	@Autowired
+	private IdmIdentityService identityService;
+	@Autowired
+	private AttachmentManager attachmentManager;
+	@Qualifier("objectMapper")
+	@Autowired
+	private ObjectMapper mapper;
+	@Autowired
+	private LoginService loginService;
+	@Autowired
+	private RoleAssignmentReportRenderer xlsxRenderer;
+	@Autowired
+	private IdmIdentityRoleService identityRoleService;
+	@Autowired
+	private IdmRoleService roleService;
+	@Autowired
+	private IdmRoleRequestService roleRequestService;
+	@Autowired
+	private IdmConceptRoleRequestService conceptRoleRequestService;
 
-    @Before
-    public void before() {
-        // report checks authorization policies - we need to log in
-        loginService.login(new LoginDto(InitTestData.TEST_ADMIN_USERNAME, new GuardedString(InitTestData.TEST_ADMIN_PASSWORD)));
-        helper.enableModule(ExtrasModuleDescriptor.MODULE_ID);
-    }
+	@Before
+	public void before() {
+		// report checks authorization policies - we need to log in
+		loginService.login(new LoginDto(InitTestData.TEST_ADMIN_USERNAME,
+                new GuardedString(InitTestData.TEST_ADMIN_PASSWORD)));
+		helper.enableModule(ExtrasModuleDescriptor.MODULE_ID);
+	}
 
-    @After
-    public void after() {
-        super.logout();
-    }
+	@After
+	public void after() {
+		super.logout();
+	}
 
-    @Test
-    @Transactional
-    public void testDisabledIdentity() throws IOException {
-        // prepare test identities
-        IdmIdentityDto identityOne = helper.createIdentity();
-        IdmIdentityDto identityDisabled = helper.createIdentity();
-        IdmRoleDto role = helper.createRole();
-        IdmIdentityContractDto primeContract = helper.getPrimeContract(identityOne);
-        helper.assignRoles(primeContract, role);
-        //
-        // prepare report filter
-        RptReportDto report = new RptReportDto(UUID.randomUUID());
-        report.setExecutorName(reportExecutor.getName());
-        //
-        // generate report
-        report = reportExecutor.generate(report);
-        Assert.assertNotNull(report.getData());
-        List<IdmIdentityDto> identityRoles = mapper.readValue(
-                attachmentManager.getAttachmentData(report.getData()),
-                new TypeReference<List<IdmIdentityDto>>() {
-                });
-        //
-        // test
-        //Assert.assertTrue(identityRoles.stream().anyMatch(i -> i.equals(identityOne)));
-        //Assert.assertFalse(identityRoles.stream().anyMatch(i -> i.equals(identityDisabled)));
-        //
-        attachmentManager.deleteAttachments(report);
-    }
+	@Test
+	@Transactional
+	public void testDisabledIdentity() throws IOException {
+		// prepare test identities
+		IdmIdentityDto identityOne = helper.createIdentity();
+		IdmIdentityDto identityDisabled = helper.createIdentity();
+		IdmRoleDto role = helper.createRole();
+		IdmIdentityContractDto primeContract = helper.getPrimeContract(identityOne);
+		helper.createRoleRequest(primeContract, role);
+		//
+		// prepare report filter
+		RptReportDto report = new RptReportDto(UUID.randomUUID());
+		report.setExecutorName(reportExecutor.getName());
+		//
+		// generate report
+		report = reportExecutor.generate(report);
+		Assert.assertNotNull(report.getData());
+		List<IdmIdentityDto> identityRoles = mapper.readValue(
+				attachmentManager.getAttachmentData(report.getData()),
+				new TypeReference<List<IdmIdentityDto>>() {
+				});
+		//
+		// test
+		//Assert.assertTrue(identityRoles.stream().anyMatch(i -> i.equals(identityOne)));
+		//Assert.assertFalse(identityRoles.stream().anyMatch(i -> i.equals(identityDisabled)));
+		//
+		attachmentManager.deleteAttachments(report);
+	}
 
-    @Test
-    @Transactional
-    public void testRenderers() {
-        IdmIdentityDto identity = helper.createIdentity();
-        IdmIdentityDto identity2 = helper.createIdentity();
-        IdmIdentityDto identity3 = helper.createIdentity();
+	@Test
+	@Transactional
+	public void testRenderers() {
+		IdmIdentityDto identity = helper.createIdentity();
+		IdmIdentityDto identity2 = helper.createIdentity();
+		IdmIdentityDto identity3 = helper.createIdentity();
 
-        IdmRoleDto role = helper.createRole();
-        IdmRoleDto role2 = helper.createRole();
+		IdmRoleDto role = helper.createRole();
+		IdmRoleDto role2 = helper.createRole();
 
-        IdmIdentityContractDto primeContract = helper.getPrimeContract(identity);
-        IdmIdentityContractDto primeContract2 = helper.getPrimeContract(identity);
-        IdmIdentityContractDto primeContract3 = helper.getPrimeContract(identity);
+		IdmIdentityContractDto primeContract = helper.getPrimeContract(identity);
+		IdmIdentityContractDto primeContract2 = helper.getPrimeContract(identity);
+		IdmIdentityContractDto primeContract3 = helper.getPrimeContract(identity);
 
-        IdmRoleRequestDto idmRoleRequestDto = helper.assignRoles(primeContract, role);
-        helper.executeRequest(idmRoleRequestDto, true, true);
+		IdmRoleRequestDto idmRoleRequestDto = helper.createRoleRequest(primeContract, role);
+		idmRoleRequestDto = roleRequestService.startRequest(idmRoleRequestDto.getId(), true);
+		roleRequestService.executeRequest(idmRoleRequestDto.getId());
 
-        idmRoleRequestDto = helper.assignRoles(primeContract2, role, role2);
-        helper.executeRequest(idmRoleRequestDto, true, true);
+		idmRoleRequestDto = helper.createRoleRequest(primeContract2, role, role2);
+		idmRoleRequestDto = roleRequestService.startRequest(idmRoleRequestDto.getId(), true);
+		roleRequestService.executeRequest(idmRoleRequestDto.getId());
 
-        idmRoleRequestDto = helper.assignRoles(primeContract3, role2);
-        helper.executeRequest(idmRoleRequestDto, true, true);
+		idmRoleRequestDto = helper.createRoleRequest(primeContract3, role2);
+		idmRoleRequestDto = roleRequestService.startRequest(idmRoleRequestDto.getId(), true);
+		roleRequestService.executeRequest(idmRoleRequestDto.getId());
 
-        //
-        // prepare report filter
-        RptReportDto report = new RptReportDto(UUID.randomUUID());
-        report.setExecutorName(reportExecutor.getName());
-        //
-        // generate report
-        report = reportExecutor.generate(report);
-        //
-        InputStream render = xlsxRenderer.render(report);
-        Assert.assertNotNull(render);
-    }
+		//
+		// prepare report filter
+		RptReportDto report = new RptReportDto(UUID.randomUUID());
+		report.setExecutorName(reportExecutor.getName());
+		//
+		// generate report
+		report = reportExecutor.generate(report);
+		//
+		InputStream render = xlsxRenderer.render(report);
+		Assert.assertNotNull(render);
+	}
+
+
+	@Test
+	@Transactional
+	public void testNullPointers() {
+		IdmIdentityDto identity = helper.createIdentity();
+		IdmIdentityDto identity2 = helper.createIdentity();
+		IdmIdentityDto identity3 = helper.createIdentity();
+
+		IdmRoleDto role = helper.createRole();
+		IdmRoleDto role2 = helper.createRole();
+
+		IdmIdentityContractDto primeContract = helper.getPrimeContract(identity);
+		IdmIdentityContractDto primeContract2 = helper.getPrimeContract(identity2);
+		IdmIdentityContractDto primeContract3 = helper.getPrimeContract(identity3);
+
+		IdmRoleRequestDto idmRoleRequestDto = helper.createRoleRequest(primeContract, role);
+		idmRoleRequestDto = roleRequestService.startRequest(idmRoleRequestDto.getId(), true);
+		roleRequestService.executeRequest(idmRoleRequestDto.getId());
+
+		idmRoleRequestDto = helper.createRoleRequest(primeContract2, role2);
+		idmRoleRequestDto = roleRequestService.startRequest(idmRoleRequestDto.getId(), true);
+		roleRequestService.executeRequest(idmRoleRequestDto.getId());
+
+		idmRoleRequestDto = helper.createRoleRequest(primeContract3, role2);
+		idmRoleRequestDto = roleRequestService.startRequest(idmRoleRequestDto.getId(), true);
+		roleRequestService.executeRequest(idmRoleRequestDto.getId());
+		// delete identity role
+		List<IdmIdentityRoleDto> allByIdentity = identityRoleService.findAllByIdentity(identity.getId());
+		Assert.assertEquals(1, allByIdentity.size());
+		identityRoleService.delete(allByIdentity.get(0));
+		// delete role
+		roleService.delete(role);
+		// delete identity
+		identityService.delete(identity);
+		//
+		// prepare report filter
+		RptReportDto report = new RptReportDto(UUID.randomUUID());
+		report.setExecutorName(reportExecutor.getName());
+		//
+		// generate report
+		report = reportExecutor.generate(report);
+		//
+		InputStream render = xlsxRenderer.render(report);
+		Assert.assertNotNull(report.getData());
+		Assert.assertNotNull(render);
+	}
+
 }
