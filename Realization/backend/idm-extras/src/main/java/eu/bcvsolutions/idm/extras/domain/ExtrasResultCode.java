15cac3e05d650945c317157a468b294a263bdd9a
Roman Kučera <roman.kuca@gmail.com>
2020-06-10 15:34:25 +0200

    #2316 Create LRT for importing code list and it's items

--- Realization/backend/idm-extras/src/main/java/eu/bcvsolutions/idm/extras/domain/ExtrasResultCode.java
+++ Realization/backend/idm-extras/src/main/java/eu/bcvsolutions/idm/extras/domain/ExtrasResultCode.java
@@ -47,8 +47,10 @@ public enum ExtrasResultCode implements ResultCode {
 	IDENTITY_ROLE_CANNOT_BE_MODIFIED(HttpStatus.BAD_REQUEST, "Role [%s] cannot be modified, you are not guarantee of the role!"),
 	EMPTY_ATTACHMENT_ID(HttpStatus.BAD_REQUEST, "Choose file to import!"),
 	CONTRACT_END_NOTIFICATION_DAYS_BEFORE(HttpStatus.BAD_REQUEST, "'Days before contract end' parameter is required and has to be number greater or equal to zero, given [%s]."),
-	NO_RECIPIENTS_FOUND(HttpStatus.BAD_REQUEST, "No recipients found! Notification will not be sent!");
-	
+	NO_RECIPIENTS_FOUND(HttpStatus.BAD_REQUEST, "No recipients found! Notification will not be sent!"),
+	IMPORT_CODE_LIST_EXECUTED(HttpStatus.OK, "[%s]"),
+	IMPORT_CODE_LIST_ERROR(HttpStatus.BAD_REQUEST, "[%s]");
+
 	private final HttpStatus status;
 	private final String message;
 
