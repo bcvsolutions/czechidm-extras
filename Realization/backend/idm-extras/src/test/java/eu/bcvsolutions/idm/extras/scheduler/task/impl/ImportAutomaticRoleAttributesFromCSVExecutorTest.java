ea6de9545f54bd79ad9d53c02533dd11f3491e76
Peter Štrunc <peter.strunc@bcvsolutions.eu>
2021-02-17 11:30:46 +0100

    Set version to 2.7.1 and fixing tests

--- Realization/backend/idm-extras/src/test/java/eu/bcvsolutions/idm/extras/scheduler/task/impl/ImportAutomaticRoleAttributesFromCSVExecutorTest.java
+++ Realization/backend/idm-extras/src/test/java/eu/bcvsolutions/idm/extras/scheduler/task/impl/ImportAutomaticRoleAttributesFromCSVExecutorTest.java
@@ -49,14 +49,14 @@ public class ImportAutomaticRoleAttributesFromCSVExecutorTest extends AbstractRo
 		createFormAttribute(eavNameNode, definition.getId());
 		createFormAttribute(eavTreeName, definition.getId());
 		//
-		getHelper().createRole("Manager-A");
+		getHelper().createRole("Manager-A2");
 		getHelper().createRole("MISTER_RL");
 		getHelper().createRole("LAY-OP");
-		getHelper().createRole("LAY-SP");
+		getHelper().createRole("LAY-SP2");
 		getHelper().createRole("LAY-BLL");
 		getHelper().createRole("AMG-BLK");
 		getHelper().createRole("ACC-CLOSE");
-		getHelper().createRole("CORE-CLOSE");
+		getHelper().createRole("CORE-CLOSE2");
 		
 		setPath(path, "importRolesTestFile02.csv");
 		IdmAttachmentDto attachment = createAttachment();
